c382d2fccd78b5c24a93fab71eb5904c9299f2a0
Vinícius Barros <vicaetano_barros@hotmail.com>
2022-07-28 13:42:40 -0300

    Started the session route

---
+++ src/components/Sessions/Sessions.js
@@ -0,0 +1,11 @@
+
+
+export default function Session({weekDay, date, time}) {
+    console.log(weekDay, date, time);
+    return (
+        <section className="session">
+
+        </section>
+    );
+}
+
