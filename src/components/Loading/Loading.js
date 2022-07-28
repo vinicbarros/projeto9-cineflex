bb3780f411067c8311b2cb2b4e6574b5057072c1
Vinícius Barros <vicaetano_barros@hotmail.com>
2022-07-28 13:42:26 -0300

    Reset css and loading gif

---
+++ src/components/Loading/Loading.js
@@ -0,0 +1,9 @@
+import Load from './spin.gif';
+
+export default function Loading() {
+    return (
+        <div className="loading">
+            <img src={Load} alt=''/>
+        </div>
+    );
+}
