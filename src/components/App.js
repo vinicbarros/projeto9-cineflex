e662053202224af2257549bb97b110dd5a931a5c
Vinícius Barros <vicaetano_barros@hotmail.com>
2022-07-28 13:41:16 -0300

    Added Routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,20 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 import './Reset.css';
+
 import Navbar from './Navbar/Navbar';
+import MainPage from './MainPage/MainPage';
+import Sessions from './SelectSession/SelectSession';
 
 export default function App() {
   return (
-    <>
+    <BrowserRouter>
     <Navbar />
-    </>
+    <Routes>
+    <Route path='/' element={<MainPage />}/>
+    <Route path='/sessoes/:idFilme' element={<Sessions/>}/>
+    </Routes>
+    </BrowserRouter>
   );
 }
 
