466294e0352d431a2d611e2da40414bdb7ed49f6
Vinícius Barros <vicaetano_barros@hotmail.com>
2022-07-31 22:20:24 -0300

    Loading page

--- src/components/Loading/Loading.js
+++ src/components/Loading/Loading.js
@@ -1,9 +1,29 @@
 import Load from './spin.gif';
+import styled from 'styled-components';
 
 export default function Loading() {
     return (
-        <div className="loading">
+        <LoadPage>
             <img src={Load} alt=''/>
-        </div>
+            <h1>Loading...</h1>
+        </LoadPage>
     );
 }
+
+const LoadPage = styled.div`
+    font-family: 'Roboto';
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    margin-inline: auto;
+
+    h1{
+    margin-top: 20px;
+    font-size: 30px;
+    font-weight: 400;
+    color: #293845;
+}
+`;
