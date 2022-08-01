dfe26a66df17f9baf041954aca0919c83c7b93d2
Vinícius Barros <vicaetano_barros@hotmail.com>
2022-07-31 22:14:21 -0300

    Added the GlobalStyle, the seats and success route

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,26 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import './Reset.css';
+import GlobalStyle from "./globalStyles";
 
-import Navbar from './Navbar/Navbar';
-import MainPage from './MainPage/MainPage';
-import Sessions from './SelectSession/SelectSession';
+import Navbar from "./Navbar";
+import MainPage from "./MainPage";
+import Sessions from "./SelectSession";
+import SelectSeats from "./SelectSeats";
+import Success from "./Success";
 
 export default function App() {
   return (
-    <BrowserRouter>
-    <Navbar />
-    <Routes>
-    <Route path='/' element={<MainPage />}/>
-    <Route path='/sessoes/:idFilme' element={<Sessions/>}/>
-    </Routes>
-    </BrowserRouter>
+    <>
+      <GlobalStyle />
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+          <Route path="/sessoes/:idFilme" element={<Sessions />} />
+          <Route path="/assentos/:idSessao" element={<SelectSeats />} />
+          <Route path="/sucesso" element={<Success />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
-
-
